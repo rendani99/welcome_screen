c3088bff06236f0df5499a205d7cfbbb47c199d0
rendania99 <rendania460@gmail.com>
2024-03-30 05:27:50 +0200

    titleContainer in dev

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Image source={require("./assets/Best.png")} style={styles.img} />
+      <View syle={styles.tittleContainer}></View>
     </View>
   );
 }
@@ -16,4 +17,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
   },
   img: { height: 200, width: 200, alignSelf: "center" },
+  tittleContainer: {},
+  title: {},
+  des: {},
+  signinButton: {},
+  signinButtonTitle: {},
+  signupButton: {},
 });
